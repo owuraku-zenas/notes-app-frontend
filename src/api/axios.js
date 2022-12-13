3d994133f1684c894aec7f8424795f9081ecb97d
Zemas Kwaku Awuku <zenasawuku@gmail.com>
2022-12-13 17:29:29 +0000

    axios config

---
+++ src/api/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+export default axios.create({
+
+    baseURL: 'http://localhost:8000/api',
+})
+
+axios.interceptors.request.use(function (config) {
+  
+    //  removing trailing slash from end of url
+    //  this little slash (which seems its called trailing), causes CORS error
+  
+    config.url = config.url.replace(/\/$/, "");
+    
+    return config;
+});
---
+++ src/api/services.js
@@ -0,0 +1,9 @@
+import axios from "./axios";
+
+// Get all Notes
+
+// Get a Note
+
+// Update a Note
+
+// Delete a Note
