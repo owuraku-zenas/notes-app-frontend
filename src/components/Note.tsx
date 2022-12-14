abb74a6e2c1c37cc69d13e93fc1121be1508fe02
Zemas Kwaku Awuku <zenasawuku@gmail.com>
2022-12-14 14:58:39 +0000

    adding props to note component(dynamic)

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,25 +1,35 @@
 import React from 'react'
 
-type Props = {}
+    type NoteType = {
+    id: number,
+    title: string,
+    description: string,
+    updated_at: string,
+    created_at: string,
+}
+
+
+type Props = {
+    note: NoteType
+}
 
 const Note = (props: Props) => {
   return (
     <div className='bg-white shadow-lg flex flex-col gap-2 rounded-md'>
                 <div className='w-full px-4 py-3 flex items-center justify-between rounded-t-md bg-blue-200'>
-                    <h2 className='text-2xl text-gray-800 font-semibold'>Title</h2>
+                    <h2 className='text-2xl text-gray-800 font-semibold'>{props.note.title}</h2>
                 </div>
-                <p className='p-2'> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus, blanditiis vitae modi praesentium sed voluptatibus vel molestias qui suscipit fugiat corrupti. Similique culpa explicabo reprehenderit quod dignissimos, tempore expedita eum.
-                    Cum corrupti laudantium fugit vel sunt ullam quia eligendi aperiam quas delectus aut enim velit temporibus, alias hic doloribus ipsum mollitia eos quo quos commodi nemo officiis explicabo possimus! Sint.
-                    Aliquam labore eum laudantium expedita aliquid, vitae, cum corrupti ea inventore quae praesentium dicta? Facilis voluptatum ut saepe assumenda! Culpa dolores quod, amet sed molestiae fugit aut natus consequatur distinctio? </p>
-                <div className='w-full flex justify-between p-2'>
+                <p className='p-2'> {props.note.description}</p>
+                <div className='w-full flex flex-col items-end p-2 text-gray-400 font-medium'>
+                    {/* FIXME: Better date conversion */}
                     <p>
-                        Created: 22/12/1999
+                        Created: {props.note.created_at}
                     </p>
                     <p>
-                        Editted: 22/12/1999
+                        Editted: {props.note.updated_at}
                     </p>
                 </div>
-                <div className='px-2 pb-2 flex justify-between'>
+                <div className='px-2 pb-2 flex flex-col gap-2 justify-between md:flex-row'>
                     <button className='px-6 py-2 bg-blue-400 rounded-sm text-gray-50 font-semibold'>Edit</button>
                     <button className='px-6 py-2 bg-red-500 rounded-sm text-gray-50 font-semibold'>Delete</button>
                 </div>
