463d6fce80c048cd86c4614a3813513ab23c3de6
Zemas Kwaku Awuku <zenasawuku@gmail.com>
2022-12-15 12:27:14 +0000

    Added interceptor to throw the 400 error messages

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,6 +5,12 @@ export default axios.create({
     baseURL: 'http://localhost:8000/api',
 })
 
+axios.interceptors.response.use(undefined, error => {
+    if(error.response.status === 400) {
+        throw error.response
+    }
+})
+
 axios.interceptors.request.use(function (config: any) {
   
     //  removing trailing slash from end of url
