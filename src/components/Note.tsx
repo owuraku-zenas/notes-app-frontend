a4b652db65fb9a59e98d5308014b7e5bfcc788a0
Zemas Kwaku Awuku <zenasawuku@gmail.com>
2022-12-16 02:18:24 +0000

    Made note collapsible

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { deleteNote } from '../api/services'
 
-    type NoteType = {
+type NoteType = {
     id: number,
     title: string,
     description: string,
@@ -17,27 +17,34 @@ type Props = {
 }
 
 const Note = (props: Props) => {
-  return (
-    <div className='bg-white shadow-lg flex flex-col gap-2 rounded-md'>
-                <div className='w-full px-4 py-3 flex items-center justify-between rounded-t-md bg-blue-200'>
-                    <h2 className='text-2xl text-gray-800 font-semibold'>{props.note.title}</h2>
-                </div>
-                <p className='p-2'> {props.note.description}</p>
-                <div className='w-full flex flex-col items-end p-2 text-gray-400 font-medium'>
-                    {/* FIXME: Better date conversion */}
-                    <p>
-                        Created: {props.note.created_at}
-                    </p>
-                    <p>
-                        Editted: {props.note.updated_at}
-                    </p>
-                </div>
-                <div className='px-2 pb-2 flex flex-col gap-2 justify-between md:flex-row'>
-                    <button className='px-6 py-2 bg-blue-400 rounded-sm text-gray-50 font-semibold' onClick={() => props.openEditor(props.note.id)} >Edit</button>
-                    <button className='px-6 py-2 bg-red-500 rounded-sm text-gray-50 font-semibold' onClick={() => {props.noteDelete(props.note.id)}} >Delete</button>
-                </div>
+    const [isOpen, setIsOpen] = useState(false)
+    return (
+        <div className='bg-white shadow-lg flex flex-col gap-2 rounded-md'>
+            <div className='w-full px-4 py-3 flex items-center justify-between cursor-pointer rounded-t-md bg-blue-200 hover:bg-blue-300' onClick={() => setIsOpen(!isOpen)}>
+                <h2 className='text-2xl text-gray-800 font-semibold'>{props.note.title}</h2>
             </div>
-  )
+            {
+                isOpen && (
+                    <>
+                        <p className='p-2'> {props.note.description}</p>
+                        <div className='w-full flex flex-col items-end p-2 text-gray-400 font-medium'>
+                            {/* FIXME: Better date conversion */}
+                            <p>
+                                Created: {props.note.created_at}
+                            </p>
+                            <p>
+                                Editted: {props.note.updated_at}
+                            </p>
+                        </div>
+                        <div className='px-2 pb-2 flex flex-col gap-2 justify-between md:flex-row'>
+                            <button className='px-6 py-2 bg-blue-400 rounded-sm text-gray-50 font-semibold' onClick={() => props.openEditor(props.note.id)} >Edit</button>
+                            <button className='px-6 py-2 bg-red-500 rounded-sm text-gray-50 font-semibold' onClick={() => { props.noteDelete(props.note.id) }} >Delete</button>
+                        </div>
+                    </>
+                )
+            }
+        </div>
+    )
 }
 
 export default Note
